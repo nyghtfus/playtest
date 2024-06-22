b27178ad31539e99b9dc5a4f4e3f6d1b59ff0919
nyghtfus <123640320+nyghtfus@users.noreply.github.com>
2024-06-22 18:11:25 -0300

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,18 @@
 FROM node:18-slim
 
-# Set working directory
+# Instalar dependências
+RUN apt-get update && \
+    apt-get install -y chromium
+
+# Definir variáveis de ambiente necessárias para o Chromium
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
+    PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
+
 WORKDIR /app
 
-# Install dependencies
 COPY package*.json ./
 RUN npm install
 
-# Copy the rest of the application
 COPY . .
 
-# Expose the port the app runs on
-EXPOSE 3000
-
-# Run the application
-CMD ["npm", "start"]
+CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const wppconnect = require('@wppconnect-team/wppconnect');
 
 wppconnect.create({
   session: 'session-name',
+  headless: true,
+  useChrome: false, // use Chromium instead of Chrome
+  browserArgs: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process', // <- necessário para containers do Docker
+    '--disable-gpu'
+  ]
+}).then(client => {
+
+
+wppconnect.create({
+  session: 'session-name',
   puppeteerOptions: {
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   }
@@ -14,3 +31,4 @@ wppconnect.create({
 }).catch((error) => {
   console.error('Error initializing WPPConnect:', error);
 });
+}).catch(error => console.log(error));
